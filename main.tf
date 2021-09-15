1de39d4470fd8a8c4f75eed59c6ab80ad8dd3a2f
phanick <39258517+phanick@users.noreply.github.com>
2021-09-15 16:52:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "google" {
      zone        = "us-central1-a"
 }
 resource "google_compute_instance" "default" {
-  name         = "test-ph-1509"
+  name         = "test-tf-demo"
   machine_type = "e2-micro"
 
   boot_disk {
